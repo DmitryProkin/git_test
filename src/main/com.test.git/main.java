11385e01b175cd61f66543cd4109c0ddb0867f63
dmitryprokin <prokindima@mail.ru>
2022-10-18 10:28:46 +0600

    4_3

--- src/main/com.test.git/main.java
+++ src/main/com.test.git/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[]args){
         System.out.println("hellosq1");
+        System.out.println("hellosq");
+        System.out.println("hellosq");
     }
 }
